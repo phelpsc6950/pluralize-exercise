03abd570cf8b3c6309014a9ca831681ccf3467aa
phelpsc6950 <phelpsc6950@connect.durhamtech.edu>
2019-05-30 21:30:08 -0400

    imported npm pluralize

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+var pluralize = require('pluralize')
+
 var words = ['dog', 'person', 'moose', 'ox', 'wife', 'guest', 'man', 'woman', 'mess', 'photo', 'avocado', 'superhero', 'bar', 'monkey', 'combo', 'toy', 'chair', 'toe'];
 
 function getPluralization(word) {
     // This needs to be replaced with the code that returns a pluralized version of the input word
+    word = pluralize(word);
     return word;
 }
 
